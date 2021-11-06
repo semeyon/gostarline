748dd0d91afcff90da0b211d66695e98a02a11b8
Semeyon S <semeyon@svetliy.ru>
2021-11-06 20:35:04 +0600

    Add arguments and basic params

--- gostarline.go
+++ gostarline.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
+	"flag"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 type RawEvent struct {
 	Type      int `json:"type"`
-	groupId   int `json:"groupId"`
+	GroupId   int `json:"groupId"`
 	Timestamp int `json:"timestamp"`
 }
 
@@ -23,6 +26,12 @@ type eventDescriptions struct {
 	Events     []EventType `json:"eventDescriptions"`
 	Code       int         `json:"code"`
 	CodeString string      `json:"codestring"`
+}
+
+type Event struct {
+	Code      int
+	Desc      string
+	Timestamp int64
 }
 
 type Balance struct {
@@ -92,8 +101,20 @@ type Data struct {
 	CodeString string    `json:"codestring"`
 }
 
+type EventsContainer struct {
+	Events     []RawEvent `json:"events"`
+	Code       int        `json:"code"`
+	CodeString string     `json:"codestring"`
+}
+
+type EventRequestParams struct {
+	Start int64 `json:"period_start"`
+	End   int64 `json:"period_end"`
+}
+
 // Get predefined events from the starline server
 func getEvents() []EventType {
+	log.Println("Request event types by https://developer.starline.ru/json/v3/library/events")
 	var eventDescriptions eventDescriptions
 
 	resp, err := http.Get("https://developer.starline.ru/json/v3/library/events")
@@ -102,52 +123,58 @@ func getEvents() []EventType {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
-	log.Println(resp.Status)
+	// log.Println(resp.Status)
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 	json.Unmarshal(bodyBytes, &eventDescriptions)
+	log.Printf("Number of event types: %v", len(eventDescriptions.Events))
 	return eventDescriptions.Events
 }
 
-// curl "https://developer.starline.ru/json/v2/device/38406090/events" --cookie 'slnet=3FE11515FA267DE4BE81594B0A8C60A5' -d '{"period_start": 1635818531, "period_end": 1635926531}'
-// func getRawEvent(deviceId string, token string, start int, end int) RawEvent {
-// 	var rawEvent RawEvent
-// 	cookie := &http.Cookie{
-// 		Name:  "slnet",
-// 		Value: token,
-// 		// Expires: 3600,
-// 	}
-// 	url := "https://developer.starline.ru/json/v3/device/" + deviceId + "/events"
-// 	log.Println(url)
-// 	req, err := http.NewRequest("GET", url, nil)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	req.AddCookie(cookie)
-// 	client := http.Client{}
-// 	resp, err := client.Do(req)
-
-// 	defer resp.Body.Close()
-// 	log.Println(resp.Status)
-// 	bodyBytes, err := ioutil.ReadAll(resp.Body)
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	json.Unmarshal(bodyBytes, &rawEvent)
-// 	return rawEvent
-// }
+func getRawEvent(deviceId string, token string, start int64, end int64) EventsContainer {
+	var eventsContainer EventsContainer
+	params := &EventRequestParams{Start: start, End: end}
+	bParams, err := json.Marshal(params)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cookie := &http.Cookie{
+		Name:  "slnet",
+		Value: token,
+	}
+	url := "https://developer.starline.ru/json/v2/device/" + deviceId + "/events"
+	log.Printf("Request raw events %s, -d %s", url, string(bParams))
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(bParams))
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.AddCookie(cookie)
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	// log.Println(resp.Status)
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	json.Unmarshal(bodyBytes, &eventsContainer)
+	log.Printf("Number of raw event: %v", len(eventsContainer.Events))
+	return eventsContainer
+}
 
 func getData(deviceId string, token string) Data {
 	var data Data
 	cookie := &http.Cookie{
 		Name:  "slnet",
 		Value: token,
-		// Expires: 3600,
 	}
 	url := "https://developer.starline.ru/json/v3/device/" + deviceId + "/data"
-	log.Println(url)
+	log.Printf("Request device data %s", url)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatal(err)
@@ -155,9 +182,11 @@ func getData(deviceId string, token string) Data {
 	req.AddCookie(cookie)
 	client := http.Client{}
 	resp, err := client.Do(req)
-
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer resp.Body.Close()
-	log.Println(resp.Status)
+	// log.Println(resp.Status)
 	bodyBytes, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
@@ -166,15 +195,40 @@ func getData(deviceId string, token string) Data {
 	return data
 }
 
+func mapEvents(eventTypes []EventType, rawEvents []RawEvent) []Event {
+	var eventsMapped []Event
+	var eventMap = make(map[int]string)
+	for _, eventType := range eventTypes {
+		eventMap[eventType.Code] = eventType.Desc
+	}
+	for _, rawEvent := range rawEvents {
+		event := Event{
+			Code:      rawEvent.Type,
+			Desc:      eventMap[rawEvent.Type],
+			Timestamp: int64(rawEvent.Timestamp),
+		}
+		eventsMapped = append(eventsMapped, event)
+	}
+	return eventsMapped
+}
+
 func main() {
-	log.Println("GoStarline")
-	slnetToken := "3FE11515FA267DE4BE81594B0A8C60A5"
-	// user_id := "1827506"
-	device_id := "38406090"
-	events := getEvents()
+	log.Println("GoStarline starting up")
+	slnetToken := flag.String("token", "", "slnet token")
+	device_id := flag.String("device_id", "38406090", "device id")
+	flag.Parse()
+
+	log.Printf("Token: %s and device id: %s will be used ", *slnetToken, *device_id)
+	eventTypes := getEvents()
+	data := getData(*device_id, *slnetToken)
+	log.Println(data)
+	apochNow := time.Now().Unix()
+	rawEvents := getRawEvent(*device_id, *slnetToken, apochNow-24*3600, apochNow)
+	// log.Println(rawEvents)
+
+	events := mapEvents(eventTypes, rawEvents.Events)
+	log.Println("---------")
 	log.Println(events)
-	data := getData(device_id, slnetToken)
-	log.Println(data.Data.Balance)
 
 	// Get events from the starline server
 }
